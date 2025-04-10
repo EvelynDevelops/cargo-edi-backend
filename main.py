78b887a35b0c8d1d779d7d870d830c9d39f808ed
yuchen <yinyuchen2000@gmail.com>
2025-04-10 17:57:50 +1000

    Add CORS middleware and route includes for EDI generation and decoding

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from api.generate_edi_route import router as edi_router
+from api.decode_edi_route import router as decode_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(edi_router)
+app.include_router(decode_router)
 
 @app.get("/")
 def read_root():
